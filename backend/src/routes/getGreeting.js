9529d52446900ced9fbdaecdd85c322b756f9dac
Ashin Walpola <96333690+AshinWalpola@users.noreply.github.com>
2024-12-22 11:06:13 +0100

    git commit -m "Update greeting API endpoint"

--- backend/src/routes/getGreeting.js
+++ backend/src/routes/getGreeting.js
@@ -1,4 +1,6 @@
-const GREETING = 'Hello world!';
+const GREETING = ["Whale hello there!",
+"Welcome to the backend of the MERN stack application!",
+"Feel free to look around and see how everything works!",];
 
 module.exports = async (req, res) => {
     res.send({
